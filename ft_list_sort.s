3267b3d6ecc5b52d51a24ba17381678fee60a6c2
Axel Lanty <axellanty14@gmail.com>
2025-10-30 21:00:00 +0100

    10 balles je smash la friteuse

--- ft_list_sort.s
+++ ft_list_sort.s
@@ -1,37 +1,55 @@
-; void ft_list_sort(t_list **begin_list, int (*cmp)());
-
 global ft_list_sort
 
-
-ft_list_sorl ; (premier noeud)
-    ;verfier si la liste est vide ou NULL 
-    test rax, rax ; verifi si la liste et NULL
-    jz .c_la_fin ; si NULL return
-
-    mov rax, [rdi] ; rdi = 1ere noeud de la list 
-    test rax, rax ; verifie si le premier noeud et vide
-    jz .c_la_fin
-
-
-
-;boucle extern - repete jusqu a aucun echange
-.boucle.externe
-    xor r8, r8 ; r8 = flag a echange =  (faux)
-    mov r9, [rdi] ;r9  current = 1ere noued de la liste
-
-;boucle interne - parcourir tous les elements de la liste
-.boucle.interne
-    mov r10, [r9 + 8] ;r10 = suivant
-    test r10, r10 ;verifie si le suivant est null
-    jz .verifie_si_echange ; si null on atteint la fin de la liste
-
-    ; apple fonction de comparaison
-    ; sauvegarder les registre avant l appel de fonction
-    ; parceque la fonction cmp peu changer les registres, a bon ? bas oui mdr hahaha
-    push rdi ; sauvegarde begin list
-    push rsi ; sauvegarde cmp (pointeur fonction)
-    push r8 ; sauvegarde le putain de drapeau 
-    push r9  ; sauvegarde du putain de courant
-    push r10 ; sauvegarde du putain de suivant
-
-    ;preparer les fils de pute d arguments pour cmp
+ft_list_sort:
+    test rdi, rdi
+    jz .fin
+    
+    mov rax, [rdi]
+    test rax, rax
+    jz .fin
+    
+.boucle_externe:
+    xor r8, r8
+    mov r9, [rdi]
+    
+.boucle_interne:
+    mov r10, [r9 + 8]
+    test r10, r10
+    jz .verifier_si_echange
+    
+    push rdi
+    push rsi
+    push r8
+    push r9
+    push r10
+    
+    mov rdi, [r9]
+    mov rsi, [r10]
+    call [rsp + 8]
+    
+    pop r10
+    pop r9
+    pop r8
+    pop rsi
+    pop rdi
+    
+    cmp eax, 0
+    jle .pas_d_echange
+    
+    mov r11, [r9]
+    mov r12, [r10]
+    mov [r9], r12
+    mov [r10], r11
+    
+    mov r8, 1
+    
+.pas_d_echange:
+    mov r9, r10
+    jmp .boucle_interne
+    
+.verifier_si_echange:
+    test r8, r8
+    jnz .boucle_externe
+    
+.fin:
+    ret
